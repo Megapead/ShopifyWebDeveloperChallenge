761d334c35d73e662f5a1df044564f40da4ecddc
Megapead <gabrielciolac@gmail.com>
2019-01-20 13:14:14 -0500

    Better Keyword Searching

--- webhook.py
+++ webhook.py
@@ -11,22 +11,29 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 @app.route('/SoftwareDeveloperChallenge', methods=['GET','POST'])
 def index():
     if request.method == 'POST':
-        print('test',file=sys.stderr)
         if request.form['itemname'] is None:
             return redirect(url_for(index)) #if there's no search data and somebody forced a POST display clean page
 
         session['search'] = request.form['itemname']
-        print('test2',file=sys.stderr)
         with open('data.json') as data_file:
             data = json.load(data_file)
-
+        json_list = []
         for item in data:
             if item["keywords"] is not None:
                 keyword_string = item["keywords"]
                 keyword_string = keyword_string.replace(',',' ')
+                keyword_string = keyword_string.replace('(',' ')
+                keyword_string = keyword_string.replace(')',' ')
+                keyword_string = keyword_string.replace('-',' ')
                 keyword_list = keyword_string.split(' ')
-                print(keyword_list)
                 if request.form['itemname'] in keyword_list:
-                    print(item["body"])
-                    return item["body"]
+                    print('strike')
+                    item["favourited"] = 'False'
+                    json_list.append(item)
+
+        if json_list is not {}:
+            #return render_template('base.html', table_search = json_list)
+            return json.dumps(json_list)
+        else:
+            return "Invalid search"
     return render_template('base.html')

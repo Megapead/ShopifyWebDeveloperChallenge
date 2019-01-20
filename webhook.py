403c00a4434e15118246edff5006d4762395c8e6
Megapead <gabrielciolac@gmail.com>
2019-01-19 20:47:07 -0500

    Search results working KW only
    Only Keywords Working right now

--- webhook.py
+++ webhook.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, url_for, session, flash, render_template, make_response
 import urllib3
 import json
+import sys
 
 app = Flask(__name__, template_folder = 'templates')
 app.config['SECRET_KEY'] = 'deve'
@@ -9,10 +10,22 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 @app.route('/SoftwareDeveloperChallenge', methods=['GET','POST'])
 def index():
-    if request.method is 'POST':
-        if request.form['search'] is None:
+    if request.method == 'POST':
+        print('test',file=sys.stderr)
+        if request.form['itemname'] is None:
             return redirect(url_for(index)) #if there's no search data and somebody forced a POST display clean page
-        session['search'] = request.args.get('search')
-        
 
-    return
+        session['search'] = request.form['itemname']
+        print('test2',file=sys.stderr)
+        with open('data.json') as data_file:
+            data = json.load(data_file)
+
+        for item in data:
+            if item["keywords"] is not None:
+                keyword_string = item["keywords"]
+                keyword_list = keyword_string.split(",")
+                print(keyword_list)
+                if request.form['itemname'] in keyword_list:
+                    print(item["body"])
+                    return item["body"]
+    return render_template('base.html')

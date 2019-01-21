cfd8315df653fcd7a679f45cc6728267430c0696
Megapead <gabrielciolac@gmail.com>
2019-01-20 19:31:25 -0500

    NW String HTML render

--- webhook.py
+++ webhook.py
@@ -1,12 +1,18 @@
-from flask import Flask, request, url_for, session, flash, render_template, make_response
+from flask import Flask, request, url_for, session, flash, render_template, make_response, Markup
 import urllib3
 import json
 import sys
+from bs4 import BeautifulSoup
+import cgi
 
 app = Flask(__name__, template_folder = 'templates')
 app.config['SECRET_KEY'] = 'deve'
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def unicodeToHTMLEntities(text):
+    return cgi.escape(text).encode('ascii', 'xmlcharrefreplace')
+
 
 @app.route('/SoftwareDeveloperChallenge', methods=['GET','POST'])
 def index():
@@ -28,12 +34,14 @@ def index():
                 keyword_list = keyword_string.split(' ')
                 if request.form['itemname'] in keyword_list:
                     print('strike')
+                    item['markup'] = Markup(item['body'])
+                    print(isinstance(item['body'], str))
                     item["favourited"] = 'False'
                     json_list.append(item)
 
         if json_list is not {}:
-            #return render_template('base.html', table_search = json_list)
-            return json.dumps(json_list)
+            return render_template('base.html', table_search = json_list)
+            #return json.dumps(json_list)
         else:
             return "Invalid search"
-    return render_template('base.html')
+    return render_template('base.html',table_search = [])
